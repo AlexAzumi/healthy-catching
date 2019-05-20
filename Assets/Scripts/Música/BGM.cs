8e39ec2b7fc5b1ef29ead0a34bfb9a3c4621947b
Alejandro Suárez <alejandro-hdez115@outlook.com>
2019-05-19 22:26:12 -0500

    BMG & nombre definitivo
    - El juego se ha renombrado a 'Healthy Catching'
    - Se incluye BGM

---
+++ Assets/Scripts/Música/BGM.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+    // Revisar si existe
+    private static GameObject _instance;
+    /*
+     * Start
+     */
+    void Awake()
+    {
+        if (!_instance)
+        {
+            _instance = this.gameObject;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this);
+    }
+}
